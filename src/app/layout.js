03a14dceab9c0ec3a3b962bbd8e66d6ee20b1127
pramodbirla01 <prmdbirla65@gmail.com>
2024-10-16 11:08:11 +0530

    hosting home

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,10 +20,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        
       </body>
     </html>
   );
